5ef75b29288b6fcbd2309ee1c9e685a87bbaa6f5
Harshit <harshitsood094@gmail.com>
2021-03-24 23:13:24 +0530

    react-create-app on Heroku

---
+++ build/precache-manifest.78c60bf96235e6508445302d91be8e94.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb3a48403f96052fe320e86a80e749da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3fa5ac507983a3b476d",
+    "url": "/static/js/2.b6ca354c.chunk.js"
+  },
+  {
+    "revision": "03ed7dafa1dd9530a40b",
+    "url": "/static/js/main.fece542a.chunk.js"
+  },
+  {
+    "revision": "31cb9d8849daaa195195",
+    "url": "/static/js/runtime-main.028bd04c.js"
+  }
+]);
